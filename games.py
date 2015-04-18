71c40d4daf65ea674333d305b5b645e4a3621b3c
Robert Rees <shudderfix@gmail.com>
2015-04-18 07:18:50 +0100

    Sorts the logs by date played

--- games.py
+++ games.py
@@ -3,11 +3,11 @@ import datetime
 import models
 
 def log(user, game_name, date_played):
-	log_entry = models.LogEntry(user=user, game_name=game_name, date_played=date_played)
+	log_entry = models.LogEntry(user=user, game_name=game_name, date_played=date_played )
 
 	log_entry.put()
 
 	return log_entry
 
 def all_played(user):
-	return models.LogEntry.query(models.LogEntry.user == user)
+	return models.LogEntry.query(models.LogEntry.user == user).order(-models.LogEntry.date_played)
