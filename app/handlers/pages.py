5fce47d0de4cf18b1273f99d6c5aafd3444f256a
Robert Rees <shudderfix@gmail.com>
2017-12-10 11:15:42 +0000

    Redirects between the home and index pages depending on whether the user is logged in or not

--- app/handlers/pages.py
+++ app/handlers/pages.py
@@ -1,9 +1,16 @@
 import flask
 
+from app import app
+
 def front_page():
+	app.logger.info(flask.session.keys())
+	if 'session_id' in flask.session:
+		return flask.redirect(flask.url_for('home'))
+
 	return flask.render_template('index.html')
 
 def home_page():
 	if not 'session_id' in flask.session:
 		return flask.redirect(flask.url_for('index'))
+
 	return flask.render_template('home.html')
