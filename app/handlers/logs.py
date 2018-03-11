9bbd3fc3a438b8eccae91dbcf4694f183f9424e9
Robert Rees <shudderfix@gmail.com>
2018-03-11 23:38:42 +0000

    Starts to log the data being passed through the logging system

--- app/handlers/logs.py
+++ app/handlers/logs.py
@@ -2,5 +2,9 @@ import flask
 
 from app import app
 
+from ..repositories import logs
+
 def log_form():
+    app.logger.info(flask.request.form)
+    logs.log(flask.request.form)
     return flask.redirect(flask.url_for('home'))
--- app/repositories/logs.py
+++ app/repositories/logs.py
@@ -1,10 +1,29 @@
+from uuid import uuid4
+
+from app import app
 
 from . import connection
 
-from .. import app
-
-_LOG = app.logger
+log_insert = """
+INSERT INTO game_logs (
+    log_id,
+    user_id,
+    game_name,
+    date_played,
+    tags,
+    notes
+)
+VALUES (
+    %(log_id)s,
+    %(user_id)s,
+    %(game_name)s,
+    %(date_played)s,
+    %(tags)s,
+    %(notes)s
+)
+"""
 
 def log(log_data):
-    _LOG.info(log_data)
+    app.logger.info(log_data)
+    log_id = uuid4()
     return None
