ecf5799e36271e7aeaff516ec910cd13c2db97f2
Robert Rees <shudderfix@gmail.com>
2018-03-13 12:44:17 +0000

    Adds a rollback and a new primary key to the log table

--- db/migrations/001-initial-table-creation.py
+++ db/migrations/001-initial-table-creation.py
@@ -8,6 +8,7 @@ __depends__ = {}
 
 create_game_log_table = """
 CREATE TABLE game_logs (
+    log_id UUID PRIMARY KEY,
     game_name text NOT NULL,
     played_on date NOT NULL,
     user_id text,
@@ -17,5 +18,8 @@ CREATE TABLE game_logs (
 """
 
 steps = [
-    step(create_game_log_table),
+    step(
+        create_game_log_table,
+        "DROP TABLE game_logs",
+        ),
 ]
