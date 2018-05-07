74186f5f7e284076b29a1654642a659510ec46ab
Robert Rees <shudderfix@gmail.com>
2018-05-07 22:51:00 +0100

    Creates the first successful cut of the import functionality

--- app/handlers/transfers.py
+++ app/handlers/transfers.py
@@ -1,9 +1,27 @@
+import json
+
 import flask
+
+from rrees_tag_manager import tags
+
+from app import users
+from app.repositories import logs
 
 def import_logs():
     return flask.render_template('import.html')
 
 def import_logs_form():
     current_user_id = users.current_user_id()
+
+    json_data = json.loads(flask.request.form.get('import_data'))
+
+    for game in json_data.get('games', []):
+        log_data = {}
+        log_data.update(game)
+        log_data.update({
+            "game_name": game['name'],
+            "tags": tags.as_string(game['tags']),
+        })
+        logs.log(current_user_id, log_data)
 
     return flask.redirect(flask.url_for('logs_listing'))
